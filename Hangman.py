0604077858683514661153e65840020d6465c22b
Lightlyflow <adensun887@gmail.com>
2020-05-26 17:38:37 -0500

    Created areas to finish.

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,30 @@
 import tkinter as tk
 
-root = tk.Tk()
-print("Hi")
 
-root.mainloop()
+class Hangman:
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Hangman")
+        self.root.geometry("700x700")
+        self.initBoard()
+        self.root.mainloop()
+
+    def initBoard(self):
+        self.createPictureFrame()
+        self.createButtonFrame()
+        self.createWordFrame()
+        return
+
+    def createPictureFrame(self):
+        return
+
+    def createButtonFrame(self):
+        return
+    def createWordFrame(self):
+        return
+
+
+if __name__ == "__main__":
+    print("Ran.")
+    game = Hangman()
